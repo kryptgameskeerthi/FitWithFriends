6c60f9b3d223d30abb58c7f764c9f1aa7164f3cc
Krypt Games <kryptgames1@gmail.com>
2019-08-06 00:45:08 +0530

    Initial commit

---
+++ app/src/main/java/com/kryptgames/health/fitwithfriends/FitWithFriendsApplication.java
@@ -0,0 +1,6 @@
+package com.kryptgames.health.fitwithfriends;
+
+import android.app.Application;
+
+public class FitWithFriendsApplication extends Application {
+}
---
+++ app/src/main/java/com/kryptgames/health/fitwithfriends/activity/WelcomeActivity.java
@@ -0,0 +1,16 @@
+package com.kryptgames.health.fitwithfriends.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kryptgames.health.fitwithfriends.R;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
