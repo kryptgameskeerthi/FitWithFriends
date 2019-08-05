cc752e89673fdd729cc185d1deca78e81f5ace06
Krypt Games <kryptgames1@gmail.com>
2019-08-06 01:53:53 +0530

    Added very basic splash activity

---
+++ app/src/main/java/com/kryptgames/health/fitwithfriends/activity/PhoneAuthenticationActivity.java
@@ -0,0 +1,17 @@
+package com.kryptgames.health.fitwithfriends.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kryptgames.health.fitwithfriends.R;
+
+
+public class PhoneAuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_authentication);
+    }
+}
--- app/src/main/java/com/kryptgames/health/fitwithfriends/activity/WelcomeActivity.java
+++ app/src/main/java/com/kryptgames/health/fitwithfriends/activity/WelcomeActivity.java
@@ -2,15 +2,35 @@ package com.kryptgames.health.fitwithfriends.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.kryptgames.health.fitwithfriends.R;
 
 public class WelcomeActivity extends AppCompatActivity {
 
+    private static final int SPLASH_TIME_OUT = 3000;// 3s
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(WelcomeActivity.this, getNextScreen());
+                startActivity(homeIntent);
+                finish();
+
+            }
+        }, SPLASH_TIME_OUT);
+    }
+
+    private Class<?> getNextScreen() {
+        return PhoneAuthenticationActivity.class;
+        // For Now Just adding the static admin screen but need to build the Logic to select appropriate screen
     }
 }
