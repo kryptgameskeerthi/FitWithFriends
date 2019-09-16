c6a20e9cf4c726c19c20146234aa1931994eeb91
Krypt Games <kryptgames1@gmail.com>
2019-09-16 15:53:37 +0530

    FIT-10: Add Rewards Fragment

--- app/src/main/java/com/kryptgames/health/fitwithfriends/activity/HomeScreenActivity.java
+++ app/src/main/java/com/kryptgames/health/fitwithfriends/activity/HomeScreenActivity.java
@@ -22,7 +22,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
+import com.kryptgames.health.fitwithfriends.BlankFragment;
+import com.kryptgames.health.fitwithfriends.CalenderFragment;
+import com.kryptgames.health.fitwithfriends.MyRewardsFragment;
+import com.kryptgames.health.fitwithfriends.Pager;
 import com.kryptgames.health.fitwithfriends.R;
+import com.kryptgames.health.fitwithfriends.RewardsRecyclerFragment;
+
+import java.util.zip.Inflater;
 
 public class HomeScreenActivity extends AppCompatActivity {
 
@@ -30,24 +37,28 @@ public class HomeScreenActivity extends AppCompatActivity {
     private ActionBar toolbar;
     private FloatingActionButton floatingActionButton;
     private TextView notificationCount;
-
-
+    private ViewPager viewPager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_drawer);
 
+        //openDialog();
 
         this.getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.activity_toolbar);
         getSupportActionBar().getCustomView();
-
+        viewPager=(ViewPager) findViewById(R.id.fwf_layout_view_pager);
         TabLayout tabLayout=(TabLayout) findViewById(R.id.fwf_layout_tab);
-        ViewPager viewPager=(ViewPager) findViewById(R.id.fwf_layout_view_pager);
+
         tabLayout.addTab(tabLayout.newTab().setText("Active"));
         tabLayout.addTab(tabLayout.newTab().setText("New"));
         tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
         tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.rust));
+
+        Pager pager = new Pager(getSupportFragmentManager(),tabLayout.getTabCount());
+        viewPager.setAdapter(pager);
+
 
 
 
@@ -77,7 +88,6 @@ public class HomeScreenActivity extends AppCompatActivity {
         toolbar = getSupportActionBar();
         BottomNavigationView navigation=(BottomNavigationView)findViewById(R.id.fwf_navigationview_bottomnavigationbar);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelelctedListener);
-
         notifications=findViewById(R.id.fwf_imagebutton_notifications);
         notifications.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -104,25 +114,45 @@ public class HomeScreenActivity extends AppCompatActivity {
         });
 
     }
+    public void selectTab(int position){
+        viewPager.setCurrentItem(position);
+    }
+
+    /*public void openDialog(){
+        InvitePopup invitePopup=new InvitePopup();
+        invitePopup.show(getSupportFragmentManager(),"example");
+    }*/
+
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelelctedListener=new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment=null;
             switch (item.getItemId())
             {
                 case R.id.fwf_navigation_home:
-                    Toast.makeText(HomeScreenActivity.this,"You have clicked on the home icon",Toast.LENGTH_SHORT).show();
-                    return true;
+                    viewPager.setVisibility(View.VISIBLE);
+                    viewPager.setCurrentItem(0);
+                    selectedFragment=new BlankFragment();
+                    break;
                 case R.id.fwf_navigation_activities:
-                    Toast.makeText(HomeScreenActivity.this,"You have clicked on the activities icon",Toast.LENGTH_SHORT).show();
-                    return true;
+                    viewPager.setVisibility(View.GONE);
+                    selectedFragment=new BlankFragment();
+                    //Toast.makeText(HomeScreenActivity.this,"You have clicked on the activities icon",Toast.LENGTH_SHORT).show();
+                    break;
                 case R.id.fwf_navigation_rewards:
-                    Toast.makeText(HomeScreenActivity.this,"You have clicked on the rewards icon",Toast.LENGTH_SHORT).show();
-                    return true;
+                    viewPager.setVisibility(View.GONE);
+                    selectedFragment=new MyRewardsFragment();
+                    //Toast.makeText(HomeScreenActivity.this,"You have clicked on the rewards icon",Toast.LENGTH_SHORT).show();
+                    break;
                 case R.id.fwf_navigation_profile:
-                    Toast.makeText(HomeScreenActivity.this,"You have clicked on the profile icon",Toast.LENGTH_SHORT).show();
-                    return true;
+                    viewPager.setVisibility(View.GONE);
+                    selectedFragment=new BlankFragment();
+                    //Toast.makeText(HomeScreenActivity.this,"You have clicked on the profile icon",Toast.LENGTH_SHORT).show();
+                    break;
             }
-            return false;
+            getSupportFragmentManager().beginTransaction().replace(R.id.main_container,selectedFragment).commit();
+            return true;
         }
     };
+
 }
